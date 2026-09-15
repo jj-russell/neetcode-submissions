3341fa7ea9d1aeb71a744addf7195a7b105554ee
jj-russell <jjrussell438@gmail.com>
2026-09-15 16:34:30 +0100

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+            
+        occurrences = {}
+
+        for letter in s:
+            if letter not in occurrences:
+                occurrences[letter] = 1
+            else:
+                occurrences[letter] += 1
+
+        for letter in t:
+            if letter not in occurrences:
+                return False
+            elif occurrences[letter] > 1:
+                occurrences[letter] -= 1
+            else:
+                occurrences.pop(letter)
+
+        if occurrences:
+            return False
+        return True
